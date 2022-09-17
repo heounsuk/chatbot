f14ebb96988581fdaba76f2913bf7fa2a093f7d5
Dennis Heo <heousuk@gmail.com>
2022-09-17 21:50:00 +0000

    ejs socket connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,32 +4,46 @@ const express = require('express');
 const { excuteQuery } = require('./db') ;
 const { serverUrl, port } = require('./config');
 
-// App
 const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 
-app.get('/add_user', (req, res) => {
-});
+// set the view engine to ejs
+app.set('view engine', 'ejs');
 
+// index page
+// page rendering codes are located in ../views/pages/
 app.get('/', (req, res) => {
-  console.log('hello world in docker')
-  res.send('Hello World to output');
+  res.render('pages/index');
 });
 
+io.on('connection', (socket)=>{
+  console.log("a user connected via socket!")
+  io.emit('chat message', 'What dessert do you like to add?');
 
-app.listen(port, () => {
-  console.log(`App listening on ${serverUrl}:${port}`);
-});
+  socket.on('disconnect', ()=>{
+      console.log("a user disconnected!")
+  })
 
-
-
-// Route to test database connection
-app.get('/desserts', async (req, res) => {
-  try {
-    let sql = `SELECT * FROM desserts`;
-    let result = await excuteQuery(sql);
-    res.send(result);
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
+  socket.on('chat message', async (msg)=>{
+    console.log("Message: "+ msg);
+    try {
+      let sql = `INSERT INTO desserts (name) VALUE (?)`;
+      let result = await excuteQuery(sql, [msg]);
+      sql = `SELECT * FROM desserts`;
+      result = await excuteQuery(sql);
+      
+      const dessert_names = result.map((object) => {
+        return object.name;
+      });
+      io.emit('chat message', dessert_names);
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  });
 })
+
+server.listen(port, () => {
+  console.log(`Server listening on ${serverUrl}:${port}`);
+});
